486fb66dea19e00c06eff9e01c60a16c0b6bdae7
Vagner Martins <va_martins3@hotmail.com>
2025-08-02 11:48:45 -0300

    Criando Retrofit; Chamando /items; Criando safeApiCall

--- app/src/main/java/com/example/minhaprimeiraapi/MainActivity.kt
+++ app/src/main/java/com/example/minhaprimeiraapi/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.minhaprimeiraapi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.minhaprimeiraapi.databinding.ActivityMainBinding
+import com.example.minhaprimeiraapi.service.RetrofitClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import safeApiCall
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +20,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fetchItems()
+    }
+
+    private fun setupView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun fetchItems() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = safeApiCall { RetrofitClient.apiService.getItems() }
+
+            Log.d("HelloWorld", "Hello $result")
+        }
     }
 }
---
+++ app/src/main/java/com/example/minhaprimeiraapi/model/Item.kt
@@ -0,0 +1,15 @@
+package com.example.minhaprimeiraapi.model
+
+data class Item(
+    val id: String,
+    val value: ItemValue
+)
+
+data class ItemValue(
+    val id: String,
+    val name: String,
+    val surname: String,
+    val profession: String,
+    val imageUrl: String,
+    val age: Int
+)

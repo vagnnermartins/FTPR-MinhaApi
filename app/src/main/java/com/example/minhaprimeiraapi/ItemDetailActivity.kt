1094ced73aa7fa36720bf80dbe53fcd45cf264c4
Vagner Martins <va_martins3@hotmail.com>
2025-08-16 10:19:10 -0300

    Correção Exercício de Delete Item; Adicionando Google Map na tela de detalhe

--- app/src/main/java/com/example/minhaprimeiraapi/ItemDetailActivity.kt
+++ app/src/main/java/com/example/minhaprimeiraapi/ItemDetailActivity.kt
@@ -4,25 +4,33 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.minhaprimeiraapi.databinding.ActivityItemDetailBinding
 import com.example.minhaprimeiraapi.model.Item
 import com.example.minhaprimeiraapi.service.Result
 import com.example.minhaprimeiraapi.service.RetrofitClient
 import com.example.minhaprimeiraapi.service.safeApiCall
 import com.example.minhaprimeiraapi.ui.loadUrl
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class ItemDetailActivity : AppCompatActivity() {
+class ItemDetailActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityItemDetailBinding
+
     private lateinit var item: Item
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +39,16 @@ class ItemDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupView()
         loadItem()
+        setupGoogleMap()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        if (::item.isInitialized) {
+            // Se o item já foi carregado por nossa chamada no BackEnd
+            // Carregue o item no Map
+            loadItemLocationInGoogleMap()
+        }
     }
 
     private fun setupView() {
@@ -40,6 +58,32 @@ class ItemDetailActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             finish()
         }
+        binding.deleteCTA.setOnClickListener {
+            deleteItem()
+        }
+    }
+
+    private fun loadItemLocationInGoogleMap() {
+        item.value.location.apply {
+            binding.googleMapContent.visibility = View.VISIBLE
+            val latLong = LatLng(latitude, longitude)
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(latLong)
+                    .title(name)
+            )
+            mMap.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    latLong,
+                    15f
+                )
+            )
+        }
+    }
+
+    private fun setupGoogleMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     private fun loadItem() {
@@ -61,11 +105,38 @@ class ItemDetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun deleteItem() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = safeApiCall { RetrofitClient.apiService.deleteItem(item.id) }
+
+            withContext(Dispatchers.Main) {
+                when (result) {
+                    is Result.Error -> {
+                        Toast.makeText(
+                            this@ItemDetailActivity,
+                            R.string.error_delete,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    is Result.Success -> {
+                        Toast.makeText(
+                            this@ItemDetailActivity,
+                            R.string.success_delete,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    }
+                }
+            }
+        }
+    }
+
     private fun handleSuccess() {
         binding.name.text = item.value.name
         binding.age.text = getString(R.string.item_age, item.value.age.toString())
         binding.profession.setText(item.value.profession)
         binding.image.loadUrl(item.value.imageUrl)
+        loadItemLocationInGoogleMap()
     }
 
     private fun handleError() {
--- app/src/main/java/com/example/minhaprimeiraapi/service/ApiService.kt
+++ app/src/main/java/com/example/minhaprimeiraapi/service/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.minhaprimeiraapi.service
 
 import com.example.minhaprimeiraapi.model.Item
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -9,4 +10,6 @@ interface ApiService {
     @GET("items") suspend fun getItems(): List<Item>
 
     @GET("items/{id}") suspend fun getItem(@Path("id") id: String): Item
+
+    @DELETE("items/{id}") suspend fun deleteItem(@Path("id") id: String)
 }
